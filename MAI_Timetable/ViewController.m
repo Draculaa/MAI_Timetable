7762e70e0e5b6dbed6a41f421e735f8bdedc4ef1
Eugene Kirtaev <evgenijkirtaev@a.local>
2015-11-07 16:36:12 +0300

    Initial Commit

---
+++ MAI_Timetable/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MAI_Timetable
+//
+//  Created by Евгений on 07.11.15.
+//  Copyright © 2015 Eugene Kirtaev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MAI_Timetable/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MAI_Timetable
+//
+//  Created by Евгений on 07.11.15.
+//  Copyright © 2015 Eugene Kirtaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
